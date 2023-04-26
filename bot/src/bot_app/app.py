abb5bce67d4be0c6fada1062993977898128546a
Roman Oryol <oryol.rd@gmail.com>
2023-04-26 19:42:50 +0300

    Включил режим MarkdownV2

--- bot/src/bot_app/app.py
+++ bot/src/bot_app/app.py
@@ -1,9 +1,10 @@
 from . local_settings import API_KEY #TODO: Добавить в git ignore
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import types
 
 #Создаем объект бота из класса, в который подкидываем наш токен
-bot = Bot(token=API_KEY)
+bot = Bot(token=API_KEY, parse_mode=types.ParseMode.MARKDOWN_V2)
 
 #Создаем объект диспетчера, в который передаем объект бота
 dp = Dispatcher(bot, storage=MemoryStorage())
--- bot/src/bot_app/translate_mode.py
+++ bot/src/bot_app/translate_mode.py
@@ -7,6 +7,7 @@ from . data_fetcher import add_word, del_word
 from . keyboards import inline_kb_YN, inline_kb_chancel, inline_kb_exit, in_kb_main_menu
 import aiogram.utils.markdown as fmt
 from emoji import emojize
+from . messages import MESSAGES
 
 @dp.message_handler(state='*', commands=['t'])
 async def process_add_word_setstate_command(message: types.Message):
@@ -15,27 +16,12 @@ async def process_add_word_setstate_command(message: types.Message):
     await state.update_data(word=message.text.lower(), user_id=message.from_user.id)
     await state.set_state(TranslateModeStates.input_word)
     try:
-        msg_text = fmt.text(
-            fmt.text(fmt.hunderline("State"),
-                     "=",
-                     await state.get_state(),
-                     "=",
-                     DeskriptionStates[await state.get_state()]),
-            fmt.text("\nСообщение: ",
-                     fmt.hitalic(
-                         "Вы вошли в режим переводчика. В нем вы будете находиться, пока не не выполните команду /end, /help или /start."),
-                     sep="\n")
-        )
+        msg_text = fmt.text(fmt.hitalic(MESSAGES['translater_mode']), sep="\n")
     except:
-        msg_text = fmt.text(
-            fmt.text(
-                     fmt.hitalic(
-                         "Вы вошли в режим переводчика. В нем вы будете находиться, пока не не выполните команду /end, /help или /start."),
-                     sep="\n")
-        )
+        msg_text = fmt.text(fmt.hitalic(MESSAGES['translater_mode']), sep="\n")
     await bot.send_message(chat_id=message.from_user.id, text=msg_text, parse_mode="HTML")
-    await bot.send_message(message.from_user.id, text=emojize('➡ Введите слово!'), reply_markup=inline_kb_exit)
-
+    await bot.send_message(message.from_user.id, text=MESSAGES['tm_in_w'], reply_markup=inline_kb_exit,
+                           parse_mode=types.ParseMode.MARKDOWN_V2)
 
 
 
@@ -69,7 +55,7 @@ async def process_add_word_add_translate_command(message: types.Message, state:F
         await bot.send_message(chat_id=message.from_user.id, text=f"-> Вы можете сделать:", reply_markup=inline_kb_YN)
         await state.set_state(TranslateModeStates.choosing_edit_mode)
     else:
-        await bot.send_message(message.from_user.id, text=emojize('➡ Введите перевод слова!'), reply_markup=inline_kb_chancel)
+        await bot.send_message(message.from_user.id, text=MESSAGES['tm_in_w'], reply_markup=inline_kb_chancel)
         await state.set_state(TranslateModeStates.input_translation)
 
 
@@ -96,7 +82,7 @@ async def process_add_word_add_translate_command(message: types.Message, state:F
         await bot.send_message(chat_id=message.from_user.id, text=f"-> Вы можете сделать:", reply_markup=inline_kb_YN)
         await state.set_state(TranslateModeStates.choosing_edit_mode)
     else:
-        await bot.send_message(message.from_user.id, text=emojize('➡ Введите слово!'))
+        await bot.send_message(message.from_user.id, text=MESSAGES['tm_in_w'], reply_markup=inline_kb_exit)
         await state.set_state(TranslateModeStates.input_word)
 
 
@@ -125,58 +111,48 @@ async def button_delete_click_call_back(callback_query: types.CallbackQuery, sta
         await bot.send_message(chat_id=data['user_id'], text=f"-> Вы можете сделать:", reply_markup=inline_kb_YN)
         await state.set_state(TranslateModeStates.choosing_edit_mode)
 
-    await bot.send_message(data['user_id'], text=emojize('➡ Введите слово!'))
+    await bot.send_message(data['user_id'], text=MESSAGES['tm_in_w'], reply_markup=inline_kb_exit, parse_mode='MarkdownV2')
     await state.set_state(TranslateModeStates.input_word)
 
 @dp.callback_query_handler(lambda c: c.data in ['go'], state=TranslateModeStates.choosing_edit_mode)
 async def button_go_click_call_back(callback_query: types.CallbackQuery, state:FSMContext):
     data = await state.get_data()
-    await bot.send_message(data['user_id'], text=emojize('➡ Введите слово!'))
+    await bot.send_message(data['user_id'], text=MESSAGES['tm_in_w'], reply_markup=inline_kb_exit)
     await state.set_state(TranslateModeStates.input_word)
 
 @dp.callback_query_handler(lambda c: c.data in ['exit'], state=[TranslateModeStates.choosing_edit_mode, TranslateModeStates.input_translation, TranslateModeStates.input_word])
 async def button_exit_click_call_back(callback_query: types.CallbackQuery, state: FSMContext):
     data = await state.get_data()
-    await state.reset_state()
+    #await state.reset_state()
     msg_text = fmt.text(fmt.text("\nСообщение: ", fmt.hitalic("Вы вышли из режима перевода. Воспользуйтесь командами /end, /help или /start."),
                  sep="\n"))
 
     await bot.send_message(chat_id=data['user_id'], text=msg_text, parse_mode="HTML", reply_markup=in_kb_main_menu)
 
 
+
 @dp.callback_query_handler(lambda c: c.data in ['chancel'], state=[TranslateModeStates.input_translation, TranslateModeStates.input_word])
 async def button_exit_click_call_back(callback_query: types.CallbackQuery, state: FSMContext):
     data = await state.get_data()
-    await bot.send_message(data['user_id'], text=emojize('➡ Введите слово!'))
+    await bot.send_message(data['user_id'], text=MESSAGES['tm_in_w'], reply_markup=inline_kb_exit)
     await state.set_state(TranslateModeStates.input_word)
 
 
 @dp.callback_query_handler(lambda c: c.data in ['translater'], state="*")
 async def button_translater_click_call_back(callback_query: types.CallbackQuery, state: FSMContext):
+
     data = await state.get_data()
+    state = dp.current_state(user=data['user_id'])
     await state.set_state(TranslateModeStates.input_word)
     try:
-        msg_text = fmt.text(
-            fmt.text(fmt.hunderline("State"),
-                     "=",
-                     await state.get_state(),
-                     "=",
-                     DeskriptionStates[await state.get_state()]),
-            fmt.text("\nСообщение: ",
-                     fmt.hitalic(
-                         "Вы вошли в режим переводчика. В нем вы будете находиться, пока не не выполните команду /end, /help или /start."),
-                     sep="\n")
-        )
+        msg_text = fmt.text(fmt.hitalic(MESSAGES['translater_mode']), sep="\n")
     except:
-        msg_text = fmt.text(
-            fmt.text(
-                     fmt.hitalic(
-                         "Вы вошли в режим переводчика. В нем вы будете находиться, пока не не выполните команду /end, /help или /start."),
-                     sep="\n")
-        )
+        msg_text = fmt.text(fmt.hitalic(MESSAGES['translater_mode']), sep="\n")
 
     await bot.send_message(chat_id=data['user_id'], text=msg_text, parse_mode="HTML")
-    await bot.send_message(chat_id=data['user_id'], text=emojize('➡ Введите слово!'), reply_markup=inline_kb_exit)
+    await bot.send_message(chat_id=data['user_id'], text=MESSAGES['tm_in_w'], reply_markup=inline_kb_exit)
+
+
 
 
 
